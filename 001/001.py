050a1abf8ac721b83e371cc85c09c61cfdfcfa97
Michael A. Colon <mialco@live.com>
2020-01-08 18:33:28 -0800

    Added problem header data

---
+++ 001/001.py
@@ -0,0 +1,12 @@
+"""
+Title: Multiples of 3 and 5
+--------------------------------------------------------------------------------
+Description:
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
+3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+--------------------------------------------------------------------------------
+Author: Michael A. Colon
+Date: 2020-JAN-08
+"""
