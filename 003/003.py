5ca483ab83c144110b40828900fae544f37d20fc
Michael A. Colon <mialco@live.com>
2020-01-09 10:16:49 -0800

    Added header data and starting code

---
+++ 003/003.py
@@ -0,0 +1,17 @@
+"""
+Title: Largest prime factor
+--------------------------------------------------------------------------------
+Description:
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+--------------------------------------------------------------------------------
+Author: Michael A. Colon
+Date: 2020-JAN-09
+"""
+def main():
+    print("Hello, world!")
+
+
+if __name__ == "__main__":
+    main()
