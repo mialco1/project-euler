fbe3deacc157a34a9fe9ff463bb0cd0bcc94cec1
Michael A. Colon <mialco@live.com>
2020-01-09 22:39:48 -0800

    Created header data and initial code for problem 5

---
+++ 005/005.py
@@ -0,0 +1,19 @@
+"""
+Title: Smallest multiple
+--------------------------------------------------------------------------------
+Description:
+2520 is the smallest number that can be divided by each of the numbers from 1 to
+10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the
+numbers from 1 to 20?
+--------------------------------------------------------------------------------
+Author: Michael A. Colon
+Date: 2020-JAN-09
+"""
+def main():
+    print("Hello, world!")
+
+
+if __name__ == "__main__":
+    main()
