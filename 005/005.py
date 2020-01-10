5312a8864e9ef131394c6f34d686b088c347c913
Michael A. Colon <mialco@live.com>
2020-01-09 23:03:34 -0800

    Implemented solution

--- 005/005.py
+++ 005/005.py
@@ -11,8 +11,25 @@ numbers from 1 to 20?
 Author: Michael A. Colon
 Date: 2020-JAN-09
 """
+def is_divisible(n):
+    for x in range (20, 0, -1):
+        if n % x != 0:
+            return False
+
+    return True
+
 def main():
-    print("Hello, world!")
+    number = 0
+    checked_value = 20
+
+    while number == 0:
+        print("Checking " + str(checked_value) + "...")
+        if is_divisible(checked_value):
+            number = checked_value
+
+        checked_value += 20
+
+    print(number)
 
 
 if __name__ == "__main__":
