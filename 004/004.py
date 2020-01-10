e56dbf09582efe9590e92003fdc075215c863b2a
Michael A. Colon <mialco@live.com>
2020-01-09 20:41:31 -0800

    Added Header data and initial code for problem 4

---
+++ 004/004.py
@@ -0,0 +1,18 @@
+"""
+Title: Largest palindrome product
+--------------------------------------------------------------------------------
+Description:
+A palindromic number reads the same both ways. The largest palindrome made from
+the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+--------------------------------------------------------------------------------
+Author: Michael A. Colon
+Date: 2020-JAN-09
+"""
+def main():
+    print("Hello, world!")
+
+
+if __name__ == "__main__":
+    main()
