c9e28982513b42c80e42ea33a2e9e9a63828dcab
Michael A. Colon <mialco@live.com>
2020-01-09 20:01:03 -0800

    Added function to check if number is prime

--- 003/003.py
+++ 003/003.py
@@ -9,6 +9,21 @@ What is the largest prime factor of the number 600851475143 ?
 Author: Michael A. Colon
 Date: 2020-JAN-09
 """
+import math
+
+def is_prime(num):
+    if(num % 2 == 0):
+        return False
+
+    # Can skip even numbers
+    # Only need to check values that are less than num / 2
+    for x in range(3, math.floor(num / 2), 2):
+        if(num % x == 0):
+            return False
+
+    return True
+
+
 def main():
     print("Hello, world!")
 
