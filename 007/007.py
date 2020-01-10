081ba3e72a4ee3935778d5ae4b3b98c1a845dc4c
Michael A. Colon <mialco@live.com>
2020-01-09 23:34:25 -0800

    Copied is_prime() from problem 3 with modification to the range()

--- 007/007.py
+++ 007/007.py
@@ -10,6 +10,19 @@ What is the 10 001st prime number?
 Author: Michael A. Colon
 Date: 2020-JAN-09
 """
+def is_prime(num):
+    if(num % 2 == 0):
+        return False
+
+    # Can skip even numbers
+    # Only need to check values that are less than num / 2
+    for x in range(2, math.floor(num / 2), 2):
+        if(num % x == 0):
+            return False
+
+    return True
+
+
 def main():
     print("Hello, world!")
 
